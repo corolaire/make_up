85e4a8d6e892990ae8619ce33e6fead3e4ab2f79
isaias sosa <isosa@castor.com>
2022-11-01 19:57:44 -0300

    decoradores y objetos GET y POST

--- maquillaje/api/views.py
+++ maquillaje/api/views.py
@@ -1,6 +1,7 @@
 
 from django.http import JsonResponse
-
+#from django.views.decorators.http import require_http_methods
 
 def saludo(request):
-    return JsonResponse({"mensaje":"Saludos"})
+    nombre = request.GET.get("nombre")
+    return JsonResponse({"mensaje":f"Saludos {nombre}"})
