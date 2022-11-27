68dc7b6842b76e5ed68ab1de53b9c38487789955
Isaias Sosa <sosaisaias250@gmail.com>
2022-11-26 21:31:41 -0300

    dsds

--- maquillaje/api/serializers.py
+++
@@ -1,5 +0,0 @@
-from rest_framework import serializers
-
-class PersonSerializers(serializers.Serializer):
-    name = serializers.CharField()
-    last_name = serializers.CharField()
--- maquillaje/api/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-
-urlpatterns = [
-    path("saludo/",views.saludo),
-    path("create/person/",views.Persona.as_view())
-]
