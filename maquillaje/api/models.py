47d034ab24441cdd32c62e65f646fcc7763b5ff4
isaias sosa <isosa@castor.com>
2022-12-13 21:53:02 -0300

    22-12-13 - Modelos

--- maquillaje/api/models.py
+++ maquillaje/api/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+from django.contrib.auth.models import User # importamos la clase user que es la clase donde se vban a logear los users por default
+
+class Productos(models.Model):
+    nombre = models.CharField(max_length=15,null=True)
+    marca = models.CharField(max_length=15,null=True)
+    cantidad = models.IntegerField()
+    user = models.ForeignKey(User,on_delete=models.CASCADE)
+    
+    class Meta:
+        db_table = "productos"
+    
+    
